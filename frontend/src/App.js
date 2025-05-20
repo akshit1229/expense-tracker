e62caf9feaa62affe4bcd685e685cb421607a43d
akshit1229 <109808465+akshit1229@users.noreply.github.com>
2025-05-20 17:38:57 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ExpenseForm from "./components/ExpenseForm";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Expense Tracker</h2>
+      <ExpenseForm />
+    </div>
+  );
+}
+
+export default App;
